7a0c61e92b53c24ce26de0321b1fecb1dfa7ba7b
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-05 19:59:01 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(num)
+  num.sort
+end
 
+def sort_array_desc(num)
+  num.sort { |x, y|
+    y <=> x
+  }
+end
+
+def sort_array_char_count(words)
+  words.sort { |a, b|
+    a.length <=> b.length
+  }
+end
+
+def swap_elements(els)
+  els[1], els[2] = els[2], els[1]
+  els
+end
+
+def reverse_array(ints)
+  ints.reverse
+end
+
+def kesha_maker(stringy)
+  stringy.each do |word|
+    word[2] = "$"
+  end
+  stringy
+end
+
+def find_a(array)
+  array.select do |words|
+    words.start_with?("a")
+  end
+end
+    
+def sum_array(nums)
+  nums.inject(0) { |ans, new|
+    ans + new
+  }
+end
+
+def add_s(final)
+  final.each_with_index.collect do |element, index|
+    if index == 1 
+      element
+    else
+      element + "s"
+    end
+  end
+end
+  
+  
